c2aba1913c7d34c8b830d5ace1f01ea47cc4432e
root <root@swooshalicio.us>
2014-12-20 23:36:17 +0000

    DB connection tweak

--- includes/settings.php
+++ includes/settings.php
@@ -1,14 +1,15 @@
 <?php
 // -- SQL --
 // hostname to connect to
-$hostname = "localhost";
+$hostname = "theblackparrot.us";
+$port = 3306
 // SQL credentials
 $user = "mpd_user";
 $password = "SOMEPASS";
 // database that stores info for the cache list
 $database = "mpd";
 // defines the SQL connection
-$mysqli = new mysqli($hostname, $user, $password, $database);
+$mysqli = new mysqli($hostname, $user, $password, $database, $port);
 
 // -- MPD --
 $mpd['host'] = "theblackparrot.us";
