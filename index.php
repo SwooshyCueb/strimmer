b992eff07770a7d311d00f17f0a0dae6350b99cf
SwooshyCueb <root@swooshalicio.us>
2014-12-21 01:07:47 +0000

    Let's turn on debugging

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
+ini_set("display_errors", "On");
+ini_set("display_startup_errors", "On");
+ini_set("error_reporting", "E_ALL");
+ini_set("html_errors", "On");
+ini_set("log_errors", "On");
+
 include "includes/settings.php";
 include "includes/functions.php";
 include_once "includes/session.php";
