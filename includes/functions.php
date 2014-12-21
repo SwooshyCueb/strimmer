28e76ef713f5fb2c8a8091d7e3a0606ca3afc7a0
SwooshyCueb <root@swooshalicio.us>
2014-12-21 11:52:56 +0000

    login.php tweaks

--- includes/functions.php
+++ includes/functions.php
@@ -3,11 +3,7 @@
 include_once dirname(__FILE__) . "/settings.php";
 
 function mpd_getPlaylist() {
-	$mpd['host'] = "theblackparrot.us";
-	$mpd['port'] = 6600;
-	$mpd['password'] = "SOMEPASS";
 	$playlist = explode("\n",str_replace("\n\r","\n",shell_exec("mpc -f \"%file%\" -h " . $mpd['host'] . " -p " . $mpd['port'] . " playlist")));
-
 	return $playlist;
 }
 
