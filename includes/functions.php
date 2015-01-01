5c89bacaf3b580555e0478d946c73de8d6723997
TheBlackParrot <theblackparrot@rocketmail.com>
2015-01-01 16:50:29 -0600

    merged getListRow

--- includes/functions.php
+++ includes/functions.php
@@ -29,61 +29,39 @@ function getListRow_Service($row) {
 	// sublime is saying /cache/ is escaped, so it's separated in case
 	// unsure if it /actually is/ but w/e
 	//<span class="oi" data-glyph="pulse"></span>
-	
-	switch ($row['SERVICE']) {
-		case 'SDCL':
-			echo '<tr class="song_row">';
-				echo '<td>';
-					echo '<img src="cache/' . $row['TRACKID'] . '.jpg" class="list_art"/>';
-					echo '<div class="list_info">';
-						echo '<div class="overflow_grd"></div>';
-						if ($_SESSION['login']) {
-							echo '<a href="includes/delete_song.php?id=' . $row['TRACKID'] . '&user=' . $_SESSION['username'] . '"><span class="oi" data-glyph="delete" id="list_delete"></span></a>';
-							echo '<a href="includes/queue_song.php?id=' . $row['TRACKID'] . '&user=' . $_SESSION['username'] . '"><span class="oi" data-glyph="pulse" id="list_queue"></span></a>';
-							// uncomment once LASTACTIVE is tracked again
-							/* if($row['ADDED_ON'] >= $_SESSION['LASTACTIVE']) {
-								echo '<span class="balloon" style="background-color: ' . $balloon_color['new'] . '; color: ' . $balloon_color['font_new'] . ';">NEW</span>';
-							} /*
 
-							// it's there when we add it, if ever
-							/* if(isset($row['ERROR'])) {
-								echo '<span class="balloon" style="background-color: ' . $balloon_color['error'] . '; color: ' . $balloon_color['font_error'] . ';">ERROR</span>';
-							} */
-						}
-						echo '<div class="list_title" style="z-index: 0;">' . $row['RETURN_ARG2'] . '</div>';
-						echo '<div class="list_artist" style="z-index: 0;"><a href="' . $row['RETURN_ARG4'] . '">' . $row['RETURN_ARG3'] . '</a></div>';
-					echo '</div>';
-				echo '</td>';
-				echo '<td>' . $row['ADDED_BY'] . '</td>';
-				// injected from includes/sections/history.php
-				if(!isset($row['PLAYED_ON']))
-					echo '<td>' . date('M. d, Y g:i A',$row['ADDED_ON']) . '</td>';
-				else
-					echo '<td>' . date('M. d, Y g:i A',$row['PLAYED_ON']) . '</td>';
-				echo '<td><img src="assets/services/soundcloud.png" class="list_svc"></td>';
-			echo '</tr>';
-			break;
-
-		case 'WYZL':
-			echo '<tr class="song_row">';
-				echo '<td>';
-					echo '<img src="cache/' . $row['TRACKID'] . '.jpg" class="list_art"/>';
-					echo '<div class="list_info">';
-						echo '<div class="overflow_grd"></div>';
-						if ($_SESSION['login']) {
-							echo '<a href="includes/delete_song.php?id=' . $row['TRACKID'] . '&user=' . $_SESSION['username'] . '"/><span class="oi" data-glyph="delete" id="list_delete"></span></a>';
-						}
-						echo '<div class="list_title" style="z-index: 0;">' . $row['RETURN_ARG2'] . '</div>';
-						echo '<div class="list_artist" style="z-index: 0;"><a href="' . $row['RETURN_ARG4'] . '">' . $row['RETURN_ARG3'] . '</a></div>';
-					echo '</div>';
-				echo '</td>';
-				echo '<td>' . $row['ADDED_BY'] . '</td>';
-				echo '<td>' . date('M. d, Y g:i A',$row['ADDED_ON']) . '</td>';
-				echo '<td><img src="assets/services/weasyl.png" class="list_svc"></td>';
-			echo '</tr>';
-			break;
-
-		default:
-			break;
+	if(!isset($row['SERVICE'])) {
+		die();
 	}
+
+	echo '<tr class="song_row">';
+		echo '<td>';
+			echo '<img src="cache/' . $row['TRACKID'] . '.jpg" class="list_art"/>';
+			echo '<div class="list_info">';
+				echo '<div class="overflow_grd"></div>';
+				if ($_SESSION['login']) {
+					echo '<a href="includes/delete_song.php?id=' . $row['TRACKID'] . '&user=' . $_SESSION['username'] . '"><span class="oi" data-glyph="delete" id="list_delete"></span></a>';
+					echo '<a href="includes/queue_song.php?id=' . $row['TRACKID'] . '&user=' . $_SESSION['username'] . '"><span class="oi" data-glyph="pulse" id="list_queue"></span></a>';
+					// uncomment once LASTACTIVE is tracked again
+					/* if($row['ADDED_ON'] >= $_SESSION['LASTACTIVE']) {
+						echo '<span class="balloon" style="background-color: ' . $balloon_color['new'] . '; color: ' . $balloon_color['font_new'] . ';">NEW</span>';
+					} /*
+
+					// it's there when we add it, if ever
+					/* if(isset($row['ERROR'])) {
+						echo '<span class="balloon" style="background-color: ' . $balloon_color['error'] . '; color: ' . $balloon_color['font_error'] . ';">ERROR</span>';
+					} */
+				}
+				echo '<div class="list_title" style="z-index: 0;">' . $row['RETURN_ARG2'] . '</div>';
+				echo '<div class="list_artist" style="z-index: 0;"><a href="' . $row['RETURN_ARG4'] . '">' . $row['RETURN_ARG3'] . '</a></div>';
+			echo '</div>';
+		echo '</td>';
+		echo '<td>' . $row['ADDED_BY'] . '</td>';
+		// injected from includes/sections/history.php
+		if(!isset($row['PLAYED_ON']))
+			echo '<td>' . date('M. d, Y g:i A',$row['ADDED_ON']) . '</td>';
+		else
+			echo '<td>' . date('M. d, Y g:i A',$row['PLAYED_ON']) . '</td>';
+		echo '<td><img src="assets/services/' . $row['SERVICE'] . '.png" class="list_svc"></td>';
+	echo '</tr>';
 }
