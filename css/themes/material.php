e3fc6bde0c8e5e1bb25bf694e8607631b8dd2810
TheBlackParrot <theblackparrot@rocketmail.com>
2015-01-25 05:24:54 -0600

    added theme: material

---
+++ css/themes/material.php
@@ -0,0 +1,66 @@
+<?php
+
+// rgb, rgba, and hex values with 3 or 6 digits are valid
+
+$font_color['body'] = "#111";
+$font_color['header'] = "#fff";
+$font_color['col1'] = "#111";
+$font_color['col1_hover'] = "#fff";
+$font_color['link_list'] = "#2196F3";
+$font_color['user'] = "#fff";
+$font_color['user_hover'] = "#fff";
+$font_color['footer'] = "#fff";
+$font_color['link_footer'] = "#BBDEFB";
+$font_color['link_list'] = "#2196F3";
+$font_color['link_list_hover'] = "#999";
+$font_color['link_footer_hover'] = "#999";
+$font_color['list_header'] = "#fff";
+$font_color['list_oddrows'] = "#000";
+$font_color['list_evenrows'] = "#000";
+$font_color['list_rows_hover'] = "#000";
+$font_color['dropdown_hover'] = "#000";
+$font_color['dialog'] = "#fff";
+$font_color['dialog_buttons'] = "#fff";
+$font_color['dialog_buttons_hover'] = "#fff";
+$font_color['dialog_buttons_clicked'] = "#fff";
+$font_color['dialog_input'] = "#fff";
+$font_color['list_delete'] = "#333";
+$font_color['list_delete_hover'] = "#555";
+
+$bg_color['header'] = "#2196F3";
+$bg_color['col1'] = "#fff";
+$bg_color['col1_hover'] = "#2196F3";
+$bg_color['user'] = "#000";
+$bg_color['user_hover'] = "#fff";
+$bg_color['footer'] = "#212121";
+$bg_color['list_header'] = "#1976D2";
+$bg_color['list_oddrows'] = "#f0f0f0";
+$bg_color['list_evenrows'] = "#fff";
+$bg_color['list_hover'] = "#eee";
+$bg_color['dropdown_hover'] = "#fff";
+$bg_color['dialog'] = "#212121";
+$bg_color['dialog_buttons'] = "#666";
+$bg_color['dialog_buttons_hover'] = "#2196F3";
+$bg_color['dialog_buttons_clicked'] = "#000";
+$bg_color['dialog_input'] = "#000";
+
+$border_color['hr'] = "rgba(0,0,0,0.15)";
+$border_color['col1'] = "#fff";
+$border_color['user'] = "#000";
+$border_color['user_hover'] = "#fff";
+$border_color['footer'] = "#212121";
+$border_color['dialog_input'] = "#212121";
+
+$service_color['soundcloud'] = "#f60";
+$service_color['soundcloud_hover'] = "#fff";
+$service_color['weasyl'] = "#980000";
+$service_color['weasyl_hover'] = "#fff";
+$service_color['jamendo'] = "#522a5b";
+$service_color['jamendo_hover'] = "#fff";
+
+$balloon_color['general'] = "#2196F3";
+$balloon_color['new'] = "#2a5";
+$balloon_color['error'] = "#f04";
+$balloon_color['font_general'] = "#fff";
+$balloon_color['font_new'] = "#fff";
+$balloon_color['font_error'] = "#fff";
