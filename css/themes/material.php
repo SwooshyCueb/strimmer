3f74afcaa373b7265c90704cf22fb93a65a5d64f
TheBlackParrot <theblackparrot@rocketmail.com>
2015-01-25 05:28:14 -0600

    couple of tweaks to material

--- css/themes/material.php
+++ css/themes/material.php
@@ -45,11 +45,11 @@ $bg_color['dialog_buttons_clicked'] = "#000";
 $bg_color['dialog_input'] = "#000";
 
 $border_color['hr'] = "rgba(0,0,0,0.15)";
-$border_color['col1'] = "#fff";
+$border_color['col1'] = "transparent";
 $border_color['user'] = "#000";
 $border_color['user_hover'] = "#fff";
-$border_color['footer'] = "#212121";
-$border_color['dialog_input'] = "#212121";
+$border_color['footer'] = "transparent";
+$border_color['dialog_input'] = "transparent";
 
 $service_color['soundcloud'] = "#f60";
 $service_color['soundcloud_hover'] = "#fff";
@@ -59,8 +59,8 @@ $service_color['jamendo'] = "#522a5b";
 $service_color['jamendo_hover'] = "#fff";
 
 $balloon_color['general'] = "#2196F3";
-$balloon_color['new'] = "#2a5";
-$balloon_color['error'] = "#f04";
+$balloon_color['new'] = "#4CAF50";
+$balloon_color['error'] = "#F44336";
 $balloon_color['font_general'] = "#fff";
 $balloon_color['font_new'] = "#fff";
 $balloon_color['font_error'] = "#fff";
